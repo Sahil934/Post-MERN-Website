170dc09c44ccd3fb91b9d7aa90be3ea315fc943b
Sahil934 <sahildhillon0903@gmail.com>
2025-05-13 19:03:42 +0530

    last

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          QuillVibe
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
